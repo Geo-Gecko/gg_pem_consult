b90b6d2ab6c7c4669cade797425cb83c61207df3
Geo Gecko - Uganda <info@geogecko.com>
2019-09-18 19:39:59 +0300

    Update

--- src/pem.js
+++ src/pem.js
@@ -4,8 +4,9 @@ function filters(filterList) {
     var _dataList = d3.select("#pem-filter-list").selectAll("p")
       .data(filterList.data);
     _dataList.enter().append("p")
-    .style("width", "40%")
-      .html(function(d) {
+      .style("width", "40%")
+      .style("margin", "auto")
+      .html(function (d) {
         return "<div><span>" + d["Name"] + "</span></div>";
       })
     _dataList.exit().remove();
@@ -15,34 +16,23 @@ function filters(filterList) {
 
   var sliders = document.getElementsByClassName('sliders');
 
-  var max = d3.max(filteredDataset, function(d) {
+  var max = d3.max(filteredDataset, function (d) {
     var value = d["EUR Value of assignment"].toString().replace(/,/g, '');
     return parseFloat(value);
   });
-  var min = d3.min(filteredDataset, function(d) {
+  var min = d3.min(filteredDataset, function (d) {
     var value = d["EUR Value of assignment"].toString().replace(/,/g, '');
     return parseFloat(value);
   });
 
-  var Start = d3.min(filteredDataset, function(d) {
+  var Start = d3.min(filteredDataset, function (d) {
     return Date.parse(d["Start Year"].toString())
   });
 
-  var End = d3.max(filteredDataset, function(d) {
+  var End = d3.max(filteredDataset, function (d) {
     return Date.parse(d["Start Year"].toString())
   });
 
-  // function timestamp(str) {
-  //   return new Date(str).getTime();
-  // }
-
-  // weekdays and months
-  // var months = [
-  //   "Jan", "Feb", "Mar",
-  //   "Apr", "May", "Jun", "Jul",
-  //   "Aug", "Sep", "Oct",
-  //   "Nov", "Dec"
-  // ];
 
   function formatDate(date) {
     return date.getFullYear();
@@ -68,13 +58,17 @@ function filters(filterList) {
       min: parseFloat(Start),
       max: parseFloat(End)
     },
+    // pips: {
+    //   mode: 'range',
+    //   density: 3
+    // },
     step: 380 * 24 * 60 * 60 * 1000,
     start: [parseFloat(Start), parseFloat(End)]
   });
 
   sliderData[1].values = [toFormat(parseFloat(Start)), toFormat(parseFloat(End))];
 
-  sliders[0].noUiSlider.on('slide', function(values, handle) {
+  sliders[0].noUiSlider.on('slide', function (values, handle) {
     sliderData[1].values = values;
     styleFeatures(sliderData);
   });
@@ -83,17 +77,23 @@ function filters(filterList) {
     start: [min, max],
     behaviour: "drag",
     connect: true,
-    step: (max - min)/1000,
+    step: (max - min) / 10000,
     range: {
       'min': min,
+      '70%': 400000,
+      '80%': 2000000,
       'max': max
     },
+    // pips: {
+    //   mode: 'range',
+    //   density: 3
+    // },
     tooltips: true,
     format: {
-      to: function(value) {
-        return value.toFixed(0);
+      to: function (value) {
+        return "€" + numberWithCommas(value.toFixed(0));
       },
-      from: function(value) {
+      from: function (value) {
         return value;
       }
     }
@@ -101,20 +101,20 @@ function filters(filterList) {
 
   sliderData[0].values = [min, max];
 
-  sliders[1].noUiSlider.on('slide', function(values, handle) {
-    sliderData[0].values = values;
+  sliders[1].noUiSlider.on('slide', function (values, handle) {
+    sliderData[0].values = [values[0].replace(/,/g, '').substr(1), values[1].replace(/,/g, '').substr(1)];
     styleFeatures(sliderData);
   });
 
-  $('#resetSliders').click(function(d) {
-    $.each(sliders, function(index, value) {
+  $('#resetSliders').click(function (d) {
+    $.each(sliders, function (index, value) {
       value.noUiSlider.reset();
     })
     resetMap();
   })
 
-  $('#filterButton').click(function(d) {
-    $.each(sliders, function(index, value) {
+  $('#filterButton').click(function (d) {
+    $.each(sliders, function (index, value) {
       value.noUiSlider.reset();
     })
     resetMap();
